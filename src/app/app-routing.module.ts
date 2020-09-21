7eb2dab4e75004971547073159c79b097d557d25
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-09-21 08:13:35 -0500

    update angular material to 9 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,26 +21,39 @@ import { Pagina6Component } from './personajes/pagina6/pagina6.component';
 import { Pagina7Component } from './personajes/pagina7/pagina7.component';
 import { Pagina8Component } from './personajes/pagina8/pagina8.component';
 import { Pagina9Component } from './personajes/pagina9/pagina9.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 const routes: Routes = [
-  {path : '', component: BannerComponent},
-  {path : 'pagina2', component: Pagina2Component},
-  {path : 'pagina3', component: Pagina3Component},
-  {path : 'pagina4', component: Pagina4Component},
-  {path : 'pagina5', component: Pagina5Component},
-  {path : 'pagina6', component: Pagina6Component},
-  {path : 'pagina7', component: Pagina7Component},
-  {path : 'pagina8', component: Pagina8Component},
-  {path : 'pagina9', component: Pagina9Component},
-  {path : 'historia', component: HistoriaComponent},
-  {path : 'integrantes-de-trabajo', component: IntegrantesDeTrabajoComponent},
-  {path : 'peliculas', component: PeliculasComponent},
-  {path : 'personas', component: PersonasComponent},
-  {path : 'planetas', component: PlanetasComponent},
-  {path : 'especies', component: EspeciesComponent},
-  {path : 'naves-espaciales', component: NavesEspacialesComponent},
-  {path : 'vehiculos', component: VehiculosComponent},
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {path : 'home', component: BannerComponent},
+      {path : 'pagina2', component: Pagina2Component},
+      {path : 'pagina3', component: Pagina3Component},
+      {path : 'pagina4', component: Pagina4Component},
+      {path : 'pagina5', component: Pagina5Component},
+      {path : 'pagina6', component: Pagina6Component},
+      {path : 'pagina7', component: Pagina7Component},
+      {path : 'pagina8', component: Pagina8Component},
+      {path : 'pagina9', component: Pagina9Component},
+      {path : 'historia', component: HistoriaComponent},
+      {path : 'integrantes-de-trabajo', component: IntegrantesDeTrabajoComponent},
+      {path : 'peliculas', component: PeliculasComponent},
+      {path : 'personas', component: PersonasComponent},
+      {path : 'planetas', component: PlanetasComponent},
+      {path : 'especies', component: EspeciesComponent},
+      {path : 'naves-espaciales', component: NavesEspacialesComponent},
+      {path : 'vehiculos', component: VehiculosComponent},
+    ]
+  },
+
   {path : '**', pathMatch: 'full', redirectTo: ''}
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { InformacionService } from '@core/services/informacion.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
